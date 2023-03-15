ec3f7e05125f33d3ec3bf0bb65feae6db01c7a54
wjlee <dearjune7@gmail.com>
2023-03-15 16:21:47 +0900

    commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:image_carousel/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  print('hello world');
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+
+}
+
